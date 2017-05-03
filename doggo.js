b754e52e9d247ee5fc72d85c1c3709c150c8b25c
Suz Campbell <suz.blandon@gmail.com>
2017-05-02 19:57:54 -0600

    js doggo basics

---
+++ doggo.js
@@ -0,0 +1,8 @@
+(function() {
+	var button = document.getElementById("doggo");
+	button.addEventListener("click", getNewDoggo);
+	function getNewDoggo() {
+		alert("it's working!");
+	}
+	debugger;
+})();
